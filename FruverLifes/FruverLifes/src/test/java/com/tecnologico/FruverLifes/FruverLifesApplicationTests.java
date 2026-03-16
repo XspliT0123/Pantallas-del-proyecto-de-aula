e47f759ad496fd6d3b269a9ab0d2d529482983b4
XspliT0123 <alcidesj2003mg@gmail.com>
2026-03-16 16:39:15 -0500

    Add files via upload

---
+++ FruverLifes/FruverLifes/src/test/java/com/tecnologico/FruverLifes/FruverLifesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tecnologico.FruverLifes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruverLifesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
